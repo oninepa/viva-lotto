a0a4bef023318501d87e1f810b5fc8a30645badc
oninepa <oninepa@gmail.com>
2026-08-29 18:07:23 +0200

    fix: add static export config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export", // 이 줄을 추가합니다.
+  images: {
+    unoptimized: true, // 이미지 최적화 에러 방지용
+  },
 };
-
 module.exports = nextConfig;
